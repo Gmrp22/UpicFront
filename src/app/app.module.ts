7b6a071713c0f7c67816ff6d03e20afd48af801c
gmrp22 <gmrp22@hotmail.com>
2021-08-25 22:56:44 -0600

    Generando carpetas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './componets/app/app.component';
+import { NavbarComponent } from './app/components/navbar/navbar.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
