4310070ad64499004863e8453781465525d59fd5
Jasmine Morales <jasmine.moralesgr@gmail.com>
2021-09-01 02:18:35 -0600

    Cancelar suscripción

--- src/app/components/ModuloSuscripciones/subscriptions/subscriptions-routing.module.ts
+++ src/app/components/ModuloSuscripciones/subscriptions/subscriptions-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CancelSubscriptionComponent } from './cancel-subscription/cancel-subscription.component';
 import { ChangeSubscriptionComponent } from './change-subscription/change-subscription.component';
 import { NewSubscriptionComponent } from './new-subscription/new-subscription.component';
 
@@ -9,6 +10,10 @@ const routes: Routes = [
     children: [ {
       path: '',
       component: NewSubscriptionComponent
+    },
+    {
+      path: 'cancelSubscription',
+      component: CancelSubscriptionComponent
     },
     {
       path: 'changeSubscription',
