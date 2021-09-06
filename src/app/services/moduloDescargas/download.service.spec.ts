28d68cfa4e1a74b2b6f119975bf155ec51b24d3c
gmrp22 <gmrp22@hotmail.com>
2021-09-06 12:39:49 -0600

    Services folders created

--- src/app/services/moduloDescargas/download.service.spec.ts
+++ src/app/services/moduloDescargas/download.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { UserService } from './user.service';
+import { DownloadService } from './download.service';
 
-describe('UserService', () => {
-  let service: UserService;
+describe('DownloadService', () => {
+  let service: DownloadService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(UserService);
+    service = TestBed.inject(DownloadService);
   });
 
   it('should be created', () => {
